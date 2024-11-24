ac6b76d26f91ca118184a34f9c71f2aa189a209c
elaina <userelaina@pm.me>
2024-11-24 12:30:01 +0000

    :white_check_mark: add test

--- test_src.py
+++ test_src.py
@@ -7,7 +7,7 @@ from src.anyplayer import get_player, VERSION, get_names, get_availables
 
 def test(p: str, a: str) -> None:
     a = ba_get(a)
-    print(a)
+    # print(a)
     player = get_player(p, a)
     print(player)
     print('is_available', player.is_available())
@@ -33,31 +33,48 @@ def test(p: str, a: str) -> None:
     print()
 
 
-__FFPLAY = 'ffplay'
-__MPV = 'mpv'
-__VLC = 'vlc'
-__MPG123 = 'mpg123'  # mp3
-__CMUS = 'cmus'
-__SIMPLEAUDIO = 'simpleaudio'  # wav
-__PYAUDIO = 'pyaudio'  # mp3+wav
-__PLAYSOUND = 'playsound'  # mp3+wav
-__AUTO = 'auto'
+# CMUS = 'cmus'
+# ELISA = 'elisa'
+# FFPLAY = 'ffplay'
+# FILEMGR = 'filemgr'
+# MPG123 = 'mpg123'  # mp3
+# MPV = 'mpv'
+# PLAYSOUND = 'playsound'  # mp3+wav
+# PYAUDIO = 'pyaudio'  # mp3+wav
+# SIMPLEAUDIO = 'simpleaudio'  # wav
+# VLC = 'vlc'
+# AUTO = 'auto'
 
 print(VERSION)
-print('get_names', get_names())
+ss = get_names()
+print('get_names', ss)
 print('get_availables', get_availables())
 print()
 
-# for i in get_names():
-#     test(i, BA_WAV)
+for i in ss:
+    time.sleep(1)
+    if i in ['mpg123', 'cmus']:
+        test(i, BA_MP3)
+    elif i in ['simpleaudio']:
+        test(i, BA_WAV)
+    elif i in ['playsound', 'pyaudio']:
+        test(i, BA_MP3)
+        test(i, BA_WAV)
+    else:
+        test(i, BA_MP4)
+        test(i, BA_MP3)
+        test(i, BA_WAV)
+    print('[%s] tested' % i)
+    print()
 
-test(__PLAYSOUND, BA_WAV)
-test(__FFPLAY, BA_WAV)
-test(__MPV, BA_WAV)
-test(__VLC, BA_WAV)
-test(__MPG123, BA_MP3)
-test(__CMUS, BA_MP3)
-test(__SIMPLEAUDIO, BA_WAV)
-test(__PYAUDIO, BA_WAV)
-test(__PLAYSOUND, BA_WAV)
-test(__AUTO, BA_WAV)
+
+# test(PLAYSOUND, BA_WAV)
+# test(FFPLAY, BA_WAV)
+# test(MPV, BA_WAV)
+# test(VLC, BA_WAV)
+# test(MPG123, BA_MP3)
+# test(CMUS, BA_MP3)
+# test(SIMPLEAUDIO, BA_WAV)
+# test(PYAUDIO, BA_WAV)
+# test(PLAYSOUND, BA_WAV)
+# test(AUTO, BA_WAV)
